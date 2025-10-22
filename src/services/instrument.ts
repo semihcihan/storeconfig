4e2a2d1042e9c9da60625d3d8d4ba3c4a70e1299
Semih Cihan <umitsemihcihan@gmail.com>
2025-10-22 18:22:11 +0300

    send app version to crash analytics

--- src/services/instrument.ts
+++ src/services/instrument.ts
@@ -1,11 +1,12 @@
 import Bugsnag from "@bugsnag/js";
+import { version } from "../../package.json";
 
 const isDevelopment = process.env.NODE_ENV == "development";
 if (!isDevelopment) {
   Bugsnag.start({
     apiKey: "9214ba2acdaeee4012c236461232c4f3",
     autoTrackSessions: false,
-    appType: "worker",
+    appVersion: version,
     logger: null,
   });
 } else {
